c8f15dc017b13c367d6b3630a127a8e1074c2b29
Taide220643 <59428815+Taide220643@users.noreply.github.com>
2021-07-19 15:24:15 +0700

    Initial commit

---
+++ src/ObjectReturnDemo.java
@@ -0,0 +1,18 @@
+class ObjectReturnDemo
+{
+    int a;
+  
+    ObjectReturnDemo(int i)
+    {
+        a = i;
+    }
+  
+    // This method returns an object
+    ObjectReturnDemo incrByTen()
+    {
+        ObjectReturnDemo temp =
+               new ObjectReturnDemo(a+10);
+        return temp;
+    }
+}
+  
